ac09cb91aa3ea5db9e1f4a987d53d4be8883346f
MilleSnille <milo.hollstrand@skola.taby.se>
2025-09-05 14:04:46 +0200

    add url

--- data/låturl.py
+++ data/låturl.py
@@ -1,15 +1,29 @@
+lista = [
+
+]
+
 lista1 = [
     "https://tunebat.com/Info/Last-To-Know-Excellence/2tFux07cQF6tGg0Az2QfOB",
-"https://tunebat.com/Info/V-rlden-utanf-r-Barbados/6W8G4Lzd6C5awcAyfoOFEG",
-"https://tunebat.com/Info/Kom-och-ta-mig-Brandsta-City-Sl-ckers/22T3LCFXjOfxkiq5JgJacE",
-"https://tunebat.com/Info/Adrenaline-Mendez/570MSgOGWpri3eG33OePzW",
-"https://tunebat.com/Info/What-Difference-Does-It-Make-Poets/3HMyUxee1wn2vDH1vEiy3e",
-"https://tunebat.com/Info/No-Hay-Nada-M-s-Javiera/6O7PDSBHvaVxbICz4IV4bR",
-"https://tunebat.com/Info/Du-och-jag-i-hela-v-rlden-Martin/6qh5ZOA6bG3aDUaaI5YCKS",
-"https://tunebat.com/Info/Sluta-Schlagerversion-Molle-Ann-Louise-Hanson/2LRFvF8OpSYg1MdrbXDY2N",
-"https://tunebat.com/Info/Never-Let-It-Go-Afro-Dite/3889abs5ecHJ2voDwEwpEZ",
-"https://tunebat.com/Info/Det-innersta-rummet-Date/18RtMPYYaGYzf4Lx9rL0wJ",
-"https://tunebat.com/Info/Hon-Kommer-Med-Solsken-sten-Med-Resten/28ghjLqAxD7g9fEmZNUsDh",
+    "https://tunebat.com/Info/V-rlden-utanf-r-Barbados/6W8G4Lzd6C5awcAyfoOFEG",
+    "https://tunebat.com/Info/Kom-och-ta-mig-Brandsta-City-Sl-ckers/22T3LCFXjOfxkiq5JgJacE",
+    "https://tunebat.com/Info/Adrenaline-Mendez/570MSgOGWpri3eG33OePzW",
+    "https://tunebat.com/Info/What-Difference-Does-It-Make-Poets/3HMyUxee1wn2vDH1vEiy3e",
+    "https://tunebat.com/Info/No-Hay-Nada-M-s-Javiera/6O7PDSBHvaVxbICz4IV4bR",
+    "https://tunebat.com/Info/Du-och-jag-i-hela-v-rlden-Martin/6qh5ZOA6bG3aDUaaI5YCKS",
+    "https://tunebat.com/Info/Sluta-Schlagerversion-Molle-Ann-Louise-Hanson/2LRFvF8OpSYg1MdrbXDY2N",
+    "https://tunebat.com/Info/Never-Let-It-Go-Afro-Dite/3889abs5ecHJ2voDwEwpEZ",
+    "https://tunebat.com/Info/Det-innersta-rummet-Date/18RtMPYYaGYzf4Lx9rL0wJ",
+    "https://tunebat.com/Info/Hon-Kommer-Med-Solsken-sten-Med-Resten/28ghjLqAxD7g9fEmZNUsDh",
+    "https://tunebat.com/Info/Hon-r-en-annan-nu-Sylvia-Vrethammar/1z847WE5Em82H4YeOuIYyX",
+    "https://tunebat.com/Info/Big-Time-Party-Hanna-Hedlund-Lina-Hedlund/4d9tEe5cqnIjgwKo54bsDq",
+    "https://tunebat.com/Info/Son-of-a-Liar-Kina-Carina-Jaarnek/6obEFYeYBIWcDM9lKLhznE",
+    "https://tunebat.com/Info/Ge-mig-himlen-f-r-en-dag-Solo/5tVNpsdBZ3jPfAA2E0X9LR",
+    "https://tunebat.com/Info/The-One-That-You-Need-Friends/134UV4cpIQgeCFIazGtVPp",
+    "https://tunebat.com/Info/Vem-e-de-du-vill-ha-Kikki-Bettan-Lotta/21FrKnbvUOiygOWvNdJRwl",
+    "https://tunebat.com/Info/Sista-andetaget-Jan-Johansen/65dBbpl8ybGdwWuKa0Hjml",
+    "https://tunebat.com/Info/Vackrare-Nu-Fredrik-W-nnman/3DD6CQTXXE9xZnZfXf3GrG",
+
+
 ]
 
 lista2= [
