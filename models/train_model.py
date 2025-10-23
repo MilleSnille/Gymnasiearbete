b620ad636e742937f57201a4b68279a80415edcd
MilleSnille <milo.hollstrand@skola.taby.se>
2025-10-23 13:53:50 +0200

    imports

--- models/train_model.py
+++ models/train_model.py
@@ -2,4 +2,5 @@ import pandas as pd
 from sklearn.model_selection import train_test_split
 from sklearn.preprocessing import StandardScaler
 from tensorflow import keras
-from tensorflow.keras import layers 
+from tensorflow.keras import layers    
+
